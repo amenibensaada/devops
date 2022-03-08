ba9dc75c6954814d862682995e2ea4e3599d23b6
amenibensaada <amenisaada19@gmail.com>
2022-03-08 14:47:02 +0100

    testtttttttttttt

--- test/test.spec.js
+++ test/test.spec.js
@@ -5,7 +5,7 @@ const app = require('../app')
 describe('Test Handlers', function () {
     test('Should return hello world', (done) => {
         request(app)
-        .get('/')
+        .get('/test/')
         .expect(200)
         .expect('Hello, World : our first test!', done)
     });
