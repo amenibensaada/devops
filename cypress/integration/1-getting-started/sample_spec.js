8a3cdd226cfbc6f8781f0bfc8f9df71d49c2e447
amenibensaada <amenisaada19@gmail.com>
2022-03-15 11:10:44 +0100

    test_cypress

--- cypress/integration/1-getting-started/sample_spec.js
+++ cypress/integration/1-getting-started/sample_spec.js
@@ -2,7 +2,7 @@ describe('My First Test', () => {
     it('Visits the page', () => {
       cy.visit('https://amenibensaadaweathery.netlify.app/')
       cy.get('*[class^="btn"]').contains('Sign In').click({ force: true })
-      cy.url().should('include', '/signin')
+      cy.url().should('include', '/hihihih')
 
     })
   })
